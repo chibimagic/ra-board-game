61fa3e30a9ef6612a1f8e81763661415fe2adbda
chibimagic <git@chibimagic.org>
2014-10-11 10:38:05 -0700

    Differentiate between initializing and creating a new player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,17 @@
 class Player
-  attr_accessor :name, :suns, :tiles
+  attr_accessor :name, :suns, :tiles, :victory_points
 
-  def initialize(name, suns, tiles)
+  def initialize(name, suns, tiles, victory_points)
     raise 'Invalid name' unless name.is_a?(String)
     raise 'Invalid suns' unless suns.is_a?(Array) && suns.length == 3 && suns.all? { |sun| sun.is_a?(Sun) }
     raise 'Invalid tiles' unless tiles.is_a?(Array) && tiles.all? { |tile| tile.is_a?(Tile) }
+    raise 'Invalid victory points' unless victory_points.is_a?(Integer)
+  end
+
+  def self.create_new(name, suns)
+    tiles = []
+    victory_points = 5
+
+    new(name, suns, tiles, victory_points)
   end
 end
