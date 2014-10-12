f666d89ea7688281195b17ac0451105ac856f814
chibimagic <git@chibimagic.org>
2014-10-12 00:50:39 -0700

    Auctions

---
+++ app/models/auction.rb
@@ -0,0 +1,19 @@
+class Auction
+  attr_reader :voluntary, :ra_player, :bids
+
+  def initialize(voluntary, ra_player, bids)
+    raise 'Invalid voluntary' unless [true, false].include?(voluntary)
+    raise 'Invalid ra_player' unless ra_player.is_a?(Integer)
+    raise 'Invalid bids' unless bids.is_a?(Array) and bids.all? { |bid| bid.is_a?(Integer) || bid.nil? }
+
+    @voluntary = voluntary
+    @ra_player = ra_player
+    @bids = bids
+  end
+
+  def self.create_new(voluntary, ra_player, player_count)
+    bids = Array.new(player_count)
+
+    new(voluntary, ra_player, bids)
+  end
+end
