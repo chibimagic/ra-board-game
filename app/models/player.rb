39a8012027112f4bd0a891eba9d4057dd84a2d42
chibimagic <git@chibimagic.org>
2014-10-18 17:37:15 -0700

    Store variables that players are initialized with

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,11 @@ class Player
     raise 'Invalid suns' unless suns.is_a?(Array) && suns.length == 3 && suns.all? { |sun| sun.is_a?(Sun) }
     raise 'Invalid tiles' unless tiles.is_a?(Array) && tiles.all? { |tile| tile.is_a?(Tile) }
     raise 'Invalid victory points' unless victory_points.is_a?(Integer)
+
+    @name = name
+    @suns = suns
+    @tiles = tiles
+    @victory_points = victory_points
   end
 
   def self.create_new(name, suns)
