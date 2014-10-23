d24d1726f9a87dce384837621fff3b916d72988b
chibimagic <git@chibimagic.org>
2014-10-23 00:45:40 -0700

    Change how auctions store data

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,19 +1,21 @@
 class Auction
-  attr_reader :voluntary, :ra_player, :bids
+  attr_reader :voluntary, :ra_player, :player_count, :bids
 
-  def initialize(voluntary, ra_player, bids)
+  def initialize(voluntary, ra_player, player_count, bids)
     raise 'Invalid voluntary' unless [true, false].include?(voluntary)
     raise 'Invalid ra player' unless ra_player.is_a?(Integer)
-    raise 'Invalid bids' unless bids.is_a?(Array) and bids.all? { |bid| bid.is_a?(Integer) || bid.nil? }
+    raise 'Invalid player count' unless player_count.is_a?(Integer)
+    raise 'Invalid bids' unless bids.is_a?(Hash) and bids.keys.all? { |bidder| bidder.is_a?(Integer) } and bids.values.all? { |value| value.is_a?(Integer) || value.nil? }
 
     @voluntary = voluntary
     @ra_player = ra_player
+    @player_count = player_count
     @bids = bids
   end
 
   def self.create_new(voluntary, ra_player, player_count)
-    bids = Array.new(player_count)
+    bids = {}
 
-    new(voluntary, ra_player, bids)
+    new(voluntary, ra_player, player_count, bids)
   end
 end
