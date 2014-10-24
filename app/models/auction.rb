61fe20c2d7e7cfbdf009f0ebbebe646462126879
chibimagic <git@chibimagic.org>
2014-10-24 09:14:40 -0700

    Implement auctions

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -18,4 +18,32 @@ class Auction
 
     new(voluntary, ra_player, player_count, bids)
   end
+
+  def max_bid
+    @bids.values.max
+  end
+
+  def winner
+    unless bids.count == player_count
+      raise 'Not everyone has bid yet'
+    end
+
+    @bids.key(max_bid)
+  end
+
+  def bid(player_index, bid)
+    if !bid.nil? && bid < max_bid
+      raise 'Bid must be higher than ' + max_bid.to_s
+    end
+
+    if @bids.has_key?(player_index)
+      raise 'Player ' + player_index.to_s + ' has already bid ' + @bids.fetch(player_index).to_s
+    end
+
+    if bid.nil? && player_index == ra_player && @voluntary && @bids.values.all? { |value| value.nil? }
+      raise 'For voluntarily invoked auctions, the ra player must bid if all other players pass'
+    end
+
+    @bids[player_index] = bid
+  end
 end
