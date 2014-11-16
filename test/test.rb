06ec52230135b8e22895ff164713a03e16098455
chibimagic <git@chibimagic.org>
2014-11-16 08:53:44 -0800

    Test player count

--- test/test.rb
+++ test/test.rb
@@ -2,6 +2,13 @@ require 'minitest/autorun'
 require_relative '../app/models/game.rb'
 
 class Test < MiniTest::Test
+  def test_player_count
+    e = assert_raises(RuntimeError) { Game.create_new(['a']) }
+    assert_equal("Can only play Ra with 2-5 players", e.message)
+    e = assert_raises(RuntimeError) { Game.create_new(['a', 'b', 'c', 'd', 'e', 'f']) }
+    assert_equal("Can only play Ra with 2-5 players", e.message)
+  end
+
   def test_game_stage
     g = Game.create_new(['a', 'b'])
     e = assert_raises(RuntimeError) { g.bid(1) }
