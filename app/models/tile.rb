da69c98c062b0120f6478154735f243a757edd50
chibimagic <git@chibimagic.org>
2014-12-19 11:29:50 -0800

    Readable names for tiles

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,82 +1,111 @@
 class Tile
+  def self.to_s
+    self::TITLE + ' tile'
+  end
 end
 
 class RaTile < Tile
+  TITLE = 'Ra'
 end
 
 class GodTile < Tile
+  TITLE = 'Ra'
 end
 
 class AnubisGodTile < GodTile
+  TITLE = 'Anubus'
 end
 
 class BastetGodTile < GodTile
+  TITLE = 'Bastet'
 end
 
 class ChnumGodTile < GodTile
+  TITLE = 'Chnum'
 end
 
 class HorusGodTile < GodTile
+  TITLE = 'Horus'
 end
 
 class SethGodTile < GodTile
+  TITLE = 'Seth'
 end
 
 class SobekGodTile < GodTile
+  TITLE = 'Sobek'
 end
 
 class ThothGodTile < GodTile
+  TITLE = 'Thoth'
 end
 
 class UtGodTile < GodTile
+  TITLE = 'Ut'
 end
 
 class PharoahTile < Tile
+  TITLE = 'Pharoah'
 end
 
 class NileTile < Tile
+  TITLE = 'Nile'
 end
 
 class FloodTile < Tile
+  TITLE = 'Flood'
 end
 
 class CivilizationTile < Tile
+  TITLE = 'Civilization'
 end
 
 class ArtCivilizationTile < CivilizationTile
+  TITLE = 'Art'
 end
 
 class AgricultureCivilizationTile < CivilizationTile
+  TITLE = 'Agriculture'
 end
 
 class ReligionCivilizationTile < CivilizationTile
+  TITLE = 'Religion'
 end
 
 class AstronomyCivilizationTile < CivilizationTile
+  TITLE = 'Astronomy'
 end
 
 class WritingCivilizationTile < CivilizationTile
+  TITLE = 'Writing'
 end
 
 class GoldTile < Tile
+  TITLE = 'Gold'
 end
 
 class MonumentTile < Tile
+  TITLE = 'Monument'
 end
 
 class FortressMonumentTile < MonumentTile
+  TITLE = 'Fortress'
 end
 
 class ObeliskMonumentTile < MonumentTile
+  TITLE = 'Obelisk'
 end
 
 class PalaceMonumentTile < MonumentTile
+  TITLE = 'Palace'
 end
 
 class PyramidMonumentTile < MonumentTile
+  TITLE = 'Pyramid'
 end
 
 class SphinxMonumentTile < MonumentTile
+  TITLE = 'Sphinx'
 end
 
 class StatuesMonumentTile < MonumentTile
@@ -93,17 +122,21 @@ class DisasterTile < Tile
 end
 
 class FuneralTile < DisasterTile
+  TITLE = 'Funeral'
   DESTROYS = [PharoahTile]
 end
 
 class DroughtTile < DisasterTile
+  TITLE = 'Drought'
   DESTROYS = [FloodTile, NileTile]
 end
 
 class UnrestTile < DisasterTile
+  TITLE = 'Unrest'
   DESTROYS = [CivilizationTile]
 end
 
 class EarthquakeTile < DisasterTile
+  TITLE = 'Earthquake'
   DESTROYS = [MonumentTile]
 end
