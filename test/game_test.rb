10a3859f9ece7c7c9e27dcb8b79af30b081d3dd4
chibimagic <git@chibimagic.org>
2014-12-23 18:54:39 -0800

    Test: Move game creation into common setup

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,4 +1,8 @@
 class GameTest < MiniTest::Test
+  def setup
+    @g = Game.create_new(['a', 'b'])
+  end
+
   def test_player_count
     e = assert_raises(RuntimeError) { Game.create_new(['a']) }
     assert_equal("Can only play Ra with 2-5 players", e.message)
@@ -7,80 +11,72 @@ class GameTest < MiniTest::Test
   end
 
   def test_stage
-    g = Game.create_new(['a', 'b'])
-    e = assert_raises(RuntimeError) { g.bid(1) }
+    e = assert_raises(RuntimeError) { @g.bid(1) }
     assert_equal("Cannot bid when there is no auction", e.message)
-    e = assert_raises(RuntimeError) { g.resolve_disaster_tile(FuneralTile, GodTile, GodTile) }
+    e = assert_raises(RuntimeError) { @g.resolve_disaster_tile(FuneralTile, GodTile, GodTile) }
     assert_equal("No disaster to resolve", e.message)
   end
 
   def test_stage_auction
-    g = Game.create_new(['a', 'b'])
-    g.invoke_ra
-    e = assert_raises(RuntimeError) { g.invoke_ra }
+    @g.invoke_ra
+    e = assert_raises(RuntimeError) { @g.invoke_ra }
     assert_equal("Cannot invoke ra when there is an auction", e.message)
-    e = assert_raises(RuntimeError) { g.draw_tile }
+    e = assert_raises(RuntimeError) { @g.draw_tile }
     assert_equal("Cannot draw tile when there is an auction", e.message)
-    e = assert_raises(RuntimeError) { g.play_god_tile(CivilizationTile) }
+    e = assert_raises(RuntimeError) { @g.play_god_tile(CivilizationTile) }
     assert_equal("Cannot play god tile when there is an auction", e.message)
-    e = assert_raises(RuntimeError) { g.resolve_disaster_tile(FuneralTile, GodTile, GodTile) }
+    e = assert_raises(RuntimeError) { @g.resolve_disaster_tile(FuneralTile, GodTile, GodTile) }
     assert_equal("No disaster to resolve", e.message)
   end
 
   def test_stage_disaster
-    g = Game.create_new(['a', 'b'])
-    g.draw_tile(FuneralTile)
-    sun_value = g.players[g.current_player].suns.first.value
-    g.invoke_ra
-    g.bid(nil)
-    g.bid(sun_value)
-    e = assert_raises(RuntimeError) { g.invoke_ra }
+    @g.draw_tile(FuneralTile)
+    sun_value = @g.players[@g.current_player].suns.first.value
+    @g.invoke_ra
+    @g.bid(nil)
+    @g.bid(sun_value)
+    e = assert_raises(RuntimeError) { @g.invoke_ra }
     assert_equal("Cannot invoke ra when there are disasters to resolve", e.message)
-    e = assert_raises(RuntimeError) { g.draw_tile }
+    e = assert_raises(RuntimeError) { @g.draw_tile }
     assert_equal("Cannot draw tile when there are disasters to resolve", e.message)
-    e = assert_raises(RuntimeError) { g.play_god_tile(CivilizationTile) }
+    e = assert_raises(RuntimeError) { @g.play_god_tile(CivilizationTile) }
     assert_equal("Cannot play god tile when there are disasters to resolve", e.message)
   end
 
   def test_auction_creation
-    g = Game.create_new(['a', 'b'])
-    assert_equal(nil, g.auction)
-    g.draw_tile(RaTile)
-    assert_instance_of(Auction, g.auction)
+    assert_equal(nil, @g.auction)
+    @g.draw_tile(RaTile)
+    assert_instance_of(Auction, @g.auction)
   end
 
   def test_bid_nonexistent_sun
-    g = Game.create_new(['a', 'b'])
-    g.invoke_ra
-    e = assert_raises(RuntimeError) { g.bid(1) }
+    @g.invoke_ra
+    e = assert_raises(RuntimeError) { @g.bid(1) }
     assert_equal("Player does not have sun with value 1", e.message)
   end
 
   def test_bid_used_sun
-    g = Game.create_new(['a', 'b'])
-    g.invoke_ra
-    sun1_value = g.players[g.current_player].suns.first.value
-    g.bid(sun1_value)
-    sun2_value = g.players[g.current_player].suns.first.value
-    g.bid(sun2_value)
-    g.invoke_ra
-    e = assert_raises(RuntimeError) { g.bid(1) }
+    @g.invoke_ra
+    sun1_value = @g.players[@g.current_player].suns.first.value
+    @g.bid(sun1_value)
+    sun2_value = @g.players[@g.current_player].suns.first.value
+    @g.bid(sun2_value)
+    @g.invoke_ra
+    e = assert_raises(RuntimeError) { @g.bid(1) }
     assert_equal("Sun with value 1 has already been used", e.message)
   end
 
   def test_god_tile_for_god_tile
-    g = Game.create_new(['a', 'b'])
-    g.draw_tile(GodTile)
-    g.players[g.current_player].tiles.push(GodTile.new)
-    e = assert_raises(RuntimeError) { g.play_god_tile(GodTile) }
+    @g.draw_tile(GodTile)
+    @g.players[@g.current_player].tiles.push(GodTile.new)
+    e = assert_raises(RuntimeError) { @g.play_god_tile(GodTile) }
     assert_equal("Cannot use god tile to take god tile", e.message)
   end
 
   def test_god_tile_no_available_tile
-    g = Game.create_new(['a', 'b'])
-    g.draw_tile(CivilizationTile)
-    g.players[g.current_player].tiles.push(GodTile.new)
-    e = assert_raises(RuntimeError) { g.play_god_tile(NileTile) }
+    @g.draw_tile(CivilizationTile)
+    @g.players[@g.current_player].tiles.push(GodTile.new)
+    e = assert_raises(RuntimeError) { @g.play_god_tile(NileTile) }
     assert_equal("No Nile tile available in the auction track", e.message)
   end
 end
